0476db590e6d9b4ef9e07b3eac0b1945b01be889
Nur Alibasyah Wiriaatmadja <nuralibasyah@gmail.com>
2023-07-23 20:52:37 +0700

    Changed stock to JII30

--- explore_page.py
+++ explore_page.py
@@ -2,25 +2,24 @@ import streamlit as st
 from datetime import date
 
 import yfinance as yf
-#from fbprophet import Prophet
-#from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
 
+st.title("JII30 Stock Prediction App")
+st.header("Explore JII30 closing price in the last 2 years")
 
-START = "2015-01-01"
-TODAY = date.today().strftime("%Y-%m-%d")
+stocks = ("ACES.JK", "ADRO.JK", "AKRA.JK", "ANTM.JK", "ASII.JK",
+          "BRIS.JK", "BRMS.JK", "BRPT.JK", "CPIN.JK", "ESSA.JK",
+          "EXCL.JK", "HRUM.JK", "ICBP.JK", "INCO.JK", "INDF.JK",
+          "INDY.JK", "INKP.JK", "INTP.JK", "ITMG.JK", "KLBF.JK",
+          "MDKA.JK", "MIKA.JK", "PGAS.JK", "PTBA.JK", "SCMA.JK",
+          "SMGR.JK", "TLKM.JK", "TPIA.JK", "UNTR.JK", "UNVR.JK")
 
-st.title("Indonesian Stock Prediction App")
-
-stocks = ("AAPL", "GOOG", "MSFT", "GME")
-selected_stocks = st.selectbox("Select dataset for prediction", stocks)
-
-n_years = st.slider("Years of prediction:", 1, 4)
-period = n_years * 365
+selected_stocks = st.selectbox("Select stock:", stocks)
 
 @st.cache_data
+
 def load_data(ticker):
-    data = yf.download(ticker, START, TODAY)
+    data = yf.download(ticker, period="2y")
     data.reset_index(inplace=True)
     return data
 
@@ -28,14 +27,14 @@ data_load_state = st.text("Load data...")
 data = load_data(selected_stocks)
 data_load_state.text("Loading data... done!")
 
-st.subheader('Raw data')
+st.subheader("Raw Data")
 st.write(data.tail())
 
 def plot_raw_data():
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open'))
     fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock_close'))
-    fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
+    fig.layout.update(xaxis_rangeslider_visible=True)
     st.plotly_chart(fig)
 
+st.subheader("Time Series Data")
 plot_raw_data()
